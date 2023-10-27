3a35a7627d809a615589575c0a0a867fe2a0966c
Juan <rigualjuan@gmail.com>
2023-10-27 17:53:57 -0300

    fix: hover effect btn

--- src/components/common/patient-card/PatientCard.tsx
+++ src/components/common/patient-card/PatientCard.tsx
@@ -26,7 +26,7 @@ const PatientCard: React.FC<Props> = ({
         <p className="text-gray-700 text-base">{description}</p>
         <a
           href={website}
-          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all ease-in-out duration-300"
         >
           Visit Website
         </a>
