b231d74d69245f6ba14737bf1cc6565e1069c6f4
Juan <rigualjuan@gmail.com>
2023-10-28 14:51:05 -0300

    fix: linting and extend small screen query

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
 };
