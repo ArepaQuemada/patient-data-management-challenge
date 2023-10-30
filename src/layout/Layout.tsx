297debef0415ea386d155e1ca07e52a5307a226c
Juan <rigualjuan@gmail.com>
2023-10-29 22:16:16 -0300

    fix: header sticky overflow content and bug with iphone

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <Header />
-      <main className="p-4 sm:px-10 overflow-auto h-screen">{children}</main>
+      <main className="p-4 sm:px-10 h-screen mt-16">{children}</main>
     </>
   );
 };
